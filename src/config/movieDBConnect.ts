b90ed7fcdd907f471516ceeab1e28c07270c88ef
Vinícius Romualdo <viniromualdo082@gmail.com>
2022-04-29 10:13:03 -0300

    Add moviedb_url

--- src/config/movieDBConnect.ts
+++ src/config/movieDBConnect.ts
@@ -1,4 +1,7 @@
 import { MovieDb } from 'moviedb-promise';
-const moviedb = new MovieDb(process.env.MOVIEDB_API_KEY!);
+const moviedb = new MovieDb(
+	process.env.MOVIEDB_API_KEY!,
+	process.env.MOVIEDB_BASE_URL
+);
 
 export default moviedb;
