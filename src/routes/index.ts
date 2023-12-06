c8b5c5e7e89ff7ae8632dbf56942e641b9538308
vini <viniromualdo082@gmail.com>
2023-12-05 22:05:59 -0300

    update ansible playbook with PM2

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,7 +7,7 @@ import moviesRoutes from './moviesRoutes';
 
 const routes = (app: Application) => {
   app.route('/').get((req, res) => {
-    res.status(200).send({ titulo: 'Backend Netflix-Clone Terraform' });
+    res.status(200).send({ titulo: 'Backend Netflix-Clone' });
   });
 
   app.use(express.json(), imageRoutes, profileRoutes, userRoutes, moviesRoutes);
